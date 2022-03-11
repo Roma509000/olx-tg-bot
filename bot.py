35f08bb78b886efd5f3f483e9df8f655f0357bba
Lielem <r3ynieasos@gmail.com>
2022-03-11 14:29:40 +0300

    second try to deploy

--- bot.py
+++ bot.py
@@ -3,14 +3,15 @@ from telebot.types import InputMediaPhoto
 import time
 from selenium_part import OlxParser
 import random
+import os
 
 BOT_TOKEN = '5231731574:AAE3d-9mImXvoAElaBvaj2b79egVnZw4x1s'
 CHANNEL_NAME = '@olx_scraper'
 OLX_URL = 'https://www.olx.ua/nedvizhimost/kvartiry/dolgosrochnaya-arenda-kvartir/chernovtsy/?search%5Bdist%5D=5'
-start_url = 'https://www.olx.ua/d/uk/obyavlenie/arenda-kvartiry-do-10000-IDOhg4R.html#000d3b0210'
+# start_url = 'https://www.olx.ua/d/uk/obyavlenie/arenda-kvartiry-do-10000-IDOhg4R.html#000d3b0210'
 
 bot = telebot.TeleBot(BOT_TOKEN)
-last_id = '661305865'
+last_id = '742960032'
 
 while True:
     result = OlxParser('https://www.olx.ua/uk/nedvizhimost/kvartiry/dolgosrochnaya-arenda-kvartir/chernovtsy/?search%5Bdist%5D=5', last_id).get()
@@ -24,11 +25,11 @@ while True:
                 media_group = []
                 caption = f'<a href="{data["url"]}">{data["title"]}</a>\n' \
                           f'\n' \
-                          f'Ціна: {data["price"]}\n' \
-                          f'Автор: {data["author"]}\n' \
-                          f'Номер телефону: {data["phoneNumber"] if data["phoneNumber"] else "*не вдалося отримати!*"}\n'
+                          f'<b>Ціна:</b> {data["price"]}\n' \
+                          f'<b>Автор:</b> {data["author"]}\n' \
+                          f'<b>Номер телефону:</b> {data["phoneNumber"] if data["phoneNumber"] else "<i>не вдалося отримати</i>"}\n'
 
-                for num, photo in enumerate(data['photosList']):
+                for num, photo in enumerate(data['photosList'][:8]):
                     media_group.append(InputMediaPhoto(photo, caption=caption if num == 0 else '', parse_mode='html'))
 
                 try:
@@ -45,9 +46,9 @@ while True:
                         chat_id=CHANNEL_NAME,
                         text=f'<a href="{data["url"]}">{data["title"]}</a>\n'
                              f'\n'
-                             f'Ціна: {data["price"]}\n'
-                             f'Автор: {data["author"]}\n'
-                             f'Номер телефону: {data["phoneNumber"] if data["phoneNumber"] else "*не вдалося отримати!*"}\n',
+                             f'<b>Ціна:</b> {data["price"]}\n'
+                             f'<b>Автор:</b> {data["author"]}\n'
+                             f'<b>Номер телефону:</b> {data["phoneNumber"] if data["phoneNumber"] else "<i>не вдалося отримати</i>"}\n',
                         parse_mode='html'
                     )
                 except Exception as ex:
@@ -56,9 +57,9 @@ while True:
                         chat_id=CHANNEL_NAME,
                         text=f'<a href="{data["url"]}">{data["title"]}</a>\n'
                              f'\n'
-                             f'Ціна: {data["price"]}\n'
-                             f'Автор: {data["author"]}\n'
-                             f'Номер телефону: {data["phoneNumber"] if data["phoneNumber"] else "*не вдалося отримати!*"}\n',
+                             f'<b>Ціна:</b> {data["price"]}\n'
+                             f'<b>Автор:</b> {data["author"]}\n'
+                             f'<b>Номер телефону:</b> {data["phoneNumber"] if data["phoneNumber"] else "<i>не вдалося отримати</i>"}\n',
                         parse_mode='html'
                     )
 
